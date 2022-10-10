99d1844cd309c4448388a7f4470f261360870b06
ASUS <k.zezgn@gmail.com>
2022-10-10 23:48:35 +0300

    c

--- src/test/java/runners/Runner.java
+++ src/test/java/runners/Runner.java
@@ -17,7 +17,7 @@ import org.junit.runner.RunWith;
         },
         features = "./src/test/resources/features",
         glue = {"stepdefinitions","hooks"},
-        //tags = "@alert_handle",
+        tags = "@alert_handle",
         dryRun = false
 )
 public class Runner {
